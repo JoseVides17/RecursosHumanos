d57b9c96815ff7de34cded6383d32d54ca8a7232
JoseVides17 <jose.videsbaron@gmail.com>
2023-11-23 02:08:36 -0500

    inicio proyecto

---
+++ src/main/java/com/vides/rh/RhApplication.java
@@ -0,0 +1,13 @@
+package com.vides.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vides/rh/RhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vides.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
