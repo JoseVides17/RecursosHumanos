032fd8d2e62897c67039a3a18a1e484fa83ef142
JoseVides17 <jose.videsbaron@gmail.com>
2023-12-02 16:02:08 -0500

    proyecto react finalizado

--- src/main/java/com/vides/rh/controllers/EmpleadoController.java
+++ src/main/java/com/vides/rh/controllers/EmpleadoController.java
@@ -3,13 +3,16 @@ package com.vides.rh.controllers;
 import com.vides.rh.excepciones.RecursoNoEncontradoExcepcion;
 import com.vides.rh.models.Empleado;
 import com.vides.rh.services.empleado.IEmpleadoService;
+import org.hibernate.id.IntegralDataTypeHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("rh-app")
@@ -40,6 +43,29 @@ public class EmpleadoController {
         if(empleado == null)
             throw new RecursoNoEncontradoExcepcion("Empleado no encontrado, id: " + id);
         return ResponseEntity.ok(empleado);
+    }
+
+    @PutMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Integer id, @RequestBody Empleado empleadoRecibido){
+        Empleado empleado = iEmpleadoService.findById(id);
+        if (empleado == null)
+            throw new RecursoNoEncontradoExcepcion("Empleado no existe id: " + id);
+        empleado.setNombre(empleadoRecibido.getNombre());
+        empleado.setDepartamento(empleadoRecibido.getDepartamento());
+        empleado.setSueldo(empleadoRecibido.getSueldo());
+        return ResponseEntity.ok(iEmpleadoService.registrar(empleado));
+    }
+
+    @DeleteMapping("/empleados/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarEmpleado(@PathVariable Integer id){
+        Empleado empleado = iEmpleadoService.findById(id);
+        if(empleado == null)
+            throw new RecursoNoEncontradoExcepcion("No encontrado.");
+        iEmpleadoService.eliminar(empleado);
+        // Json {"Eliminado", "true"}
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
     }
 
 }
