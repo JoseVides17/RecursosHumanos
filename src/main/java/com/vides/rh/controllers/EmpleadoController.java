c8770cb4b734f4609a44608bfd5e5766a555bcc7
JoseVides17 <jose.videsbaron@gmail.com>
2023-12-02 14:09:52 -0500

    proyecto react

--- src/main/java/com/vides/rh/controllers/EmpleadoController.java
+++ src/main/java/com/vides/rh/controllers/EmpleadoController.java
@@ -1,14 +1,13 @@
 package com.vides.rh.controllers;
 
+import com.vides.rh.excepciones.RecursoNoEncontradoExcepcion;
 import com.vides.rh.models.Empleado;
 import com.vides.rh.services.empleado.IEmpleadoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +26,20 @@ public class EmpleadoController {
         var empleados = iEmpleadoService.listar();
         empleados.forEach((empleado -> logger.info(empleado.toString())));
         return empleados;
+    }
+
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado){
+        logger.info("Empleado a registrar: " + empleado);
+        return iEmpleadoService.registrar(empleado);
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> findById(@PathVariable Integer id){
+        Empleado empleado = iEmpleadoService.findById(id);
+        if(empleado == null)
+            throw new RecursoNoEncontradoExcepcion("Empleado no encontrado, id: " + id);
+        return ResponseEntity.ok(empleado);
     }
 
 }
